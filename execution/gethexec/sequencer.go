d0f63f6c62bb4b72cb7d916d3713575b6008ca04
Diego Ximenes <dxmendes1@gmail.com>
2025-04-07 16:00:02 -0300

    createBlock Doesn't block if sequencer is paused

--- execution/gethexec/sequencer.go
+++ execution/gethexec/sequencer.go
@@ -849,14 +849,11 @@ func (s *Sequencer) getForwarder(ctx context.Context) (*TxForwarder, error) {
 }
 
 // only called from createBlock, may be paused
-func (s *Sequencer) handleInactive(ctx context.Context, queueItems []txQueueItem) bool {
-	forwarder, err := s.getForwarder(ctx)
-	if err != nil {
-		return true
-	}
+func (s *Sequencer) handleInactive(ctx context.Context, forwarder *TxForwarder, queueItems []txQueueItem) {
 	if forwarder == nil {
-		return false
+		return
 	}
+
 	publishResults := make(chan *txQueueItem, len(queueItems))
 	for _, item := range queueItems {
 		item := item
@@ -878,7 +875,6 @@ func (s *Sequencer) handleInactive(ctx context.Context, queueItems []txQueueItem
 	}
 	// Evict any leftover nonce failures, forwarding them
 	s.nonceFailures.Clear()
-	return true
 }
 
 var sequencerInternalError = errors.New("sequencer internal error")
@@ -1002,6 +998,16 @@ func (s *Sequencer) precheckNonces(queueItems []txQueueItem, totalBlockSize int)
 }
 
 func (s *Sequencer) createBlock(ctx context.Context) (sequencedMsg *execution.SequencedMsg, returnValue bool) {
+	pause, forwarder := s.GetPauseAndForwarder()
+	if pause != nil {
+		select {
+		case <-pause:
+		default:
+			// doesn't block if it is paused
+			return nil, false
+		}
+	}
+
 	var queueItems []txQueueItem
 	var totalBlockSize int
 
@@ -1165,9 +1171,7 @@ func (s *Sequencer) createBlock(ctx context.Context) (sequencedMsg *execution.Se
 		return nil, false
 	}
 
-	if s.handleInactive(ctx, queueItems) {
-		return nil, false
-	}
+	s.handleInactive(ctx, forwarder, queueItems)
 
 	timestamp := time.Now().Unix()
 	s.L1BlockAndTimeMutex.Lock()
@@ -1222,12 +1226,7 @@ func (s *Sequencer) createBlock(ctx context.Context) (sequencedMsg *execution.Se
 	}
 	if errors.Is(err, execution.ErrRetrySequencer) {
 		log.Warn("error sequencing transactions", "err", err)
-		// we changed roles
-		// forward if we have where to
-		if s.handleInactive(ctx, queueItems) {
-			return nil, false
-		}
-		// try to add back to queue otherwise
+		// we changed roles, add back to the queue
 		for _, item := range queueItems {
 			s.txRetryQueue.Push(item)
 		}
