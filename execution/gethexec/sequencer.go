4a6ee19e87fecae0a2cb5f8693b134ab737b5428
Diego Ximenes <dxmendes1@gmail.com>
2025-04-10 22:19:04 -0300

    Resets lastCreatedBlockInfo when re-adding transactions to the sequencer's queue

--- execution/gethexec/sequencer.go
+++ execution/gethexec/sequencer.go
@@ -1347,6 +1347,7 @@ func (s *Sequencer) ReAddTransactionsFromLastCreatedBlock(ctx context.Context) e
 	for _, item := range s.lastCreatedBlockInfo.queueItems {
 		s.txRetryQueue.Push(item)
 	}
+	s.lastCreatedBlockInfo = nil
 	return nil
 }
 
