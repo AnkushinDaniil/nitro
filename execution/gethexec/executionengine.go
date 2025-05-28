c9477437515caa4558f1dc85e948823d23aa5157
Diego Ximenes <dxmendes1@gmail.com>
2025-05-28 10:29:05 -0300

    Consensus wait for maintenance to complete in execution side

--- execution/gethexec/executionengine.go
+++ execution/gethexec/executionengine.go
@@ -1150,3 +1150,9 @@ func (s *ExecutionEngine) TriggerMaintenance(capLimit uint64) {
 		}
 	})
 }
+
+func (s *ExecutionEngine) MaintenanceStatus() *execution.MaintenanceStatus {
+	return &execution.MaintenanceStatus{
+		IsRunning: s.runningMaintenance.Load(),
+	}
+}
